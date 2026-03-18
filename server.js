8202a8ae961ba5c96dd70d91d2d90cefc0df8934
Miguel Lazcano <alconblack11@gmail.com>
2026-03-17 19:44:22 -0700

    server

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ const startServer = async () => {
     }
 
     const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => {
-      console.log(`Servidor corriendo en puerto ${PORT}`);
-    });
+    app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
   } catch (err) {
     console.error("❌ Error al iniciar servidor:", err);
   }
